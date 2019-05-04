e41960f6cb24db9e96034265b7fc5a478c5e946b
Kc314 <kylec.314@gmail.com>
2019-05-03 20:43:55 -0700

    Adjusted destination input validation: now immediately tells if destination is not valid (old version waited until after entering transport type) Adjusted indentation throughout code; fixed majority of minor error flags

--- shortest_path.py
+++ shortest_path.py
@@ -1,95 +1,103 @@
 from collections import defaultdict, deque
 
 
-
 class Graph(object):
-   def __init__(self):
-       self.nodes = set()
-       self.edges = defaultdict(list)
-       self.distances = {}
+    def __init__(self):
+        self.nodes = set()
+        self.edges = defaultdict(list)
+        self.distances = {}
 
-   def add_node(self, value):
-       self.nodes.add(value)
+    def add_node(self, value):
+        self.nodes.add(value)
 
-   def add_edge(self, from_node, to_node, distance):
-       self.edges[from_node].append(to_node)
-       self.edges[to_node].append(from_node)
-       self.distances[(from_node, to_node)] = distance
+    def add_edge(self, from_node, to_node, distance):
+        self.edges[from_node].append(to_node)
+        self.edges[to_node].append(from_node)
+        self.distances[(from_node, to_node)] = distance
 
 
 def dijkstra(graph, initial):
-   visited = {initial: 0}
-   path = {}
+    visited = {initial: 0}
+    path = {}
 
-   nodes = set(graph.nodes)
+    nodes = set(graph.nodes)
 
-   while nodes:
-       min_node = None
-       for node in nodes:
-           if node in visited:
-               if min_node is None:
-                   min_node = node
-               elif visited[node] < visited[min_node]:
-                   min_node = node
-       if min_node is None:
-           break
+    while nodes:
+        min_node = None
+        for node in nodes:
+            if node in visited:
+                if min_node is None:
+                    min_node = node
+                elif visited[node] < visited[min_node]:
+                    min_node = node
+        if min_node is None:
+            break
 
-       nodes.remove(min_node)
-       current_weight = visited[min_node]
+        nodes.remove(min_node)
+        current_weight = visited[min_node]
 
-       for edge in graph.edges[min_node]:
-           try:
-               weight = current_weight + graph.distances[(min_node, edge)]
-           except:
-               continue
-           if edge not in visited or weight < visited[edge]:
-               visited[edge] = weight
-               path[edge] = min_node
+        for edge in graph.edges[min_node]:
+            try:
+                weight = current_weight + graph.distances[(min_node, edge)]
+            except:
+                continue
+            if edge not in visited or weight < visited[edge]:
+                visited[edge] = weight
+                path[edge] = min_node
 
-   return visited, path
+    return visited, path
 
 
 def shortest_path(graph, origin, destination):
-   visited, paths = dijkstra(graph, origin)
-   full_path = deque()
-   destination_ = paths[destination]
+    visited, paths = dijkstra(graph, origin)
+    full_path = deque()
+    destination_ = paths[destination]
 
-   while destination_ != origin:
-       full_path.appendleft(destination_ )
-       destination_  = paths[destination_ ]
+    while destination_ != origin:
+        full_path.appendleft(destination_ )
+        destination_ = paths[destination_ ]
 
-   full_path.appendleft(origin)
+    full_path.appendleft(origin)
    
-   full_path.append(destination)
+    full_path.append(destination)
 
-   return visited[destination], list(full_path)
+    return visited[destination], list(full_path)
+
 
 if __name__ == '__main__':
-   graph = Graph()
+    graph = Graph()
 
-   nodelist = ['San Jose', 'Milpitas', 'Mountain View', 'Fremont', 'Palo Alto', 'Hayward', 'South SF']
-   for node in nodelist:
-       graph.add_node(node)
+    nodelist = ['San Jose', 'Milpitas', 'Mountain View', 'Fremont', 'Palo Alto', 'Hayward', 'South SF']
+    for node in nodelist:
+        graph.add_node(node)
        
 
-var=1
-while var==1:
-   user=input('Where would you like to go?\n1: Milpitas\n2: Mountain View\n3: Fremont\n4: Palo Alto\n5: Hayward\n6: South SF\nDestination: ')
-   user1= input('Will you be [D]riving, taking the [B]us or the [T]rain?\n')
+var = 1
+while var == 1:
+    print('Where would you like to go?\n1: Milpitas\n2: Mountain View\n3: Fremont\n4: Palo Alto\n5: Hayward\n')
+    user = input('6: South SF\nDestination: ')
+    try:
+        if int(user[0]) < 1 or int(user[0]) > 6:
+            print('That is not a valid destination')
+            continue
+    except:
+        if user not in nodelist:
+            print('That is not a valid destination')
+            continue
 
-   if user1[0] in {'d', 'D'}:
-    graph.add_edge('San Jose', 'Milpitas', 10)
-    graph.add_edge('San Jose', 'Mountain View', 20)
-    graph.add_edge('Milpitas', 'Fremont', 15)
-    graph.add_edge('Mountain View', 'Fremont', 30)
-    graph.add_edge('Milpitas', 'Palo Alto', 50)
-    graph.add_edge('Fremont', 'Palo Alto', 30)
-    graph.add_edge('Palo Alto', 'Hayward', 5)
-    graph.add_edge('Hayward', 'South SF', 2)
+    user1 = input('Will you be [D]riving, taking the [B]us or the [T]rain?\n')
 
-    #print(shortest_path(graph, 'San Jose', user))
+    if user1[0] in {'d', 'D'}:
+        graph.add_edge('San Jose', 'Milpitas', 10)
+        graph.add_edge('San Jose', 'Mountain View', 20)
+        graph.add_edge('Milpitas', 'Fremont', 15)
+        graph.add_edge('Mountain View', 'Fremont', 30)
+        graph.add_edge('Milpitas', 'Palo Alto', 50)
+        graph.add_edge('Fremont', 'Palo Alto', 30)
+        graph.add_edge('Palo Alto', 'Hayward', 5)
+        graph.add_edge('Hayward', 'South SF', 2)
 
-   elif user1[0] in {'b', 'B'}:
+    elif user1[0] in {'b', 'B'}:
         graph.add_edge('San Jose', 'Milpitas', 10)
         graph.add_edge('San Jose', 'Mountain View', 20)
         graph.add_edge('Milpitas', 'Fremont', 15)
@@ -99,8 +107,7 @@ while var==1:
         graph.add_edge('Palo Alto', 'Hayward', 5)
         graph.add_edge('Hayward', 'South SF', 2)
 
-        #print(shortest_path(graph, 'San Jose', user))
-   elif user1[0] in {'t', 'T'}:
+    elif user1[0] in {'t', 'T'}:
         graph.add_edge('San Jose', 'Milpitas', 10)
         graph.add_edge('San Jose', 'Mountain View', 20)
         graph.add_edge('Milpitas', 'Fremont', 15)
@@ -110,15 +117,10 @@ while var==1:
         graph.add_edge('Palo Alto', 'Hayward', 5)
         graph.add_edge('Hayward', 'South SF', 2)
 
-   else:
-       print('That is not a valid input')
-       continue
-   try:
-       if int(user[0]) > 0 and int(user[0]) < 7:
-           print(shortest_path(graph, 'San Jose', nodelist[int(user[0])]))
-       else:
-           print('That is not a valid destination')
-   except:
-       print(shortest_path(graph, 'San Jose', user))
-
-
+    else:
+        print('That is not a valid input')
+        continue
+    try:
+        print(shortest_path(graph, 'San Jose', nodelist[int(user[0])]))
+    except:
+        print(shortest_path(graph, 'San Jose', user))
